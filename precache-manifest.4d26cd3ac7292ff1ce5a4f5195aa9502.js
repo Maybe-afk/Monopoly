7a911b0a8e876217126e746fdfaa5fb3033a1972
Maybe <kubapredki1@gmail.com>
2021-12-23 20:01:18 +0100

    Updates

---
+++ precache-manifest.4d26cd3ac7292ff1ce5a4f5195aa9502.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecb413a3f515bdc3ac81347babeb19e4",
+    "url": "/Monopoly/index.html"
+  },
+  {
+    "revision": "18de0197b36c0a26cd78",
+    "url": "/Monopoly/static/css/main.d6937961.chunk.css"
+  },
+  {
+    "revision": "667d2f0b0ec8046f76c7",
+    "url": "/Monopoly/static/js/2.3b4f2756.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/Monopoly/static/js/2.3b4f2756.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18de0197b36c0a26cd78",
+    "url": "/Monopoly/static/js/main.e2f89a87.chunk.js"
+  },
+  {
+    "revision": "4aeb7acea0915166cdb2",
+    "url": "/Monopoly/static/js/runtime-main.96e3d9ee.js"
+  }
+]);
